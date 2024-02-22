6acf5f1b97a0a610027e356e37491ee6bdced915
happydev0110 <happydev0110@gmail.com>
2024-02-22 16:48:29 -0600

    update portfolio

--- src/components/Projects/Projects.constants.jsx
+++ src/components/Projects/Projects.constants.jsx
@@ -1,39 +1,39 @@
 import webLink from '../../assets/images/links/globe2.svg';
 import dribbbleLink from '../../assets/images/links/dribbble.svg';
-import gitLink from '../../assets/images/links/github.svg';
-import ytLink from '../../assets/images/links/youtube.svg';
+// import gitLink from '../../assets/images/links/github.svg';
+// import ytLink from '../../assets/images/links/youtube.svg';
 
-import jdsoftware from '../../assets/images/projects/jdsoftware.png';
-import portfolio from '../../assets/images/projects/portfolio.png';
-import livrator from '../../assets/images/projects/livrator.png';
-import ghicicuvantul from '../../assets/images/projects/gc.png';
-import cryptocurrency from '../../assets/images/projects/cc.png';
-import traveldeck from '../../assets/images/projects/td.png';
-import xboxdash from '../../assets/images/projects/xd.png';
+// import jdsoftware from '../../assets/images/projects/jdsoftware.png';
+// import portfolio from '../../assets/images/projects/portfolio.png';
+// import livrator from '../../assets/images/projects/livrator.png';
+// import ghicicuvantul from '../../assets/images/projects/gc.png';
+// import cryptocurrency from '../../assets/images/projects/cc.png';
+// import traveldeck from '../../assets/images/projects/td.png';
+// import xboxdash from '../../assets/images/projects/xd.png';
 
-import geex from '../../assets/images/projects/geex.png';
-import tla from '../../assets/images/projects/tla.png';
-import arbachelor from '../../assets/images/projects/bachelor.png';
-import arlgtv from '../../assets/images/projects/arlg.png';
-import ardissertation from '../../assets/images/projects/dissertation.png';
-import arop7 from '../../assets/images/projects/arop7.png';
-import castleescape from '../../assets/images/projects/castleescape.png';
+// import geex from '../../assets/images/projects/geex.png';
+// import tla from '../../assets/images/projects/tla.png';
+// import arbachelor from '../../assets/images/projects/bachelor.png';
+// import arlgtv from '../../assets/images/projects/arlg.png';
+// import ardissertation from '../../assets/images/projects/dissertation.png';
+// import arop7 from '../../assets/images/projects/arop7.png';
+// import castleescape from '../../assets/images/projects/castleescape.png';
 
 import smarthome from '../../assets/images/projects/smarthome.png';
 import godofwar from '../../assets/images/projects/godofwar.png';
 import itlogo from '../../assets/images/projects/intimelogo.png';
 import nyoologo from '../../assets/images/projects/ntoologo.png';
-import btnanim from '../../assets/images/projects/btnAnim.png';
-import navui from '../../assets/images/projects/navui.png';
+// import btnanim from '../../assets/images/projects/btnAnim.png';
+// import navui from '../../assets/images/projects/navui.png';
 import mplogo from '../../assets/images/projects/mplogo.png';
 import discoverro from '../../assets/images/projects/discover-ro.png';
 import vclp from '../../assets/images/projects/vclp.png';
 import vibenow from '../../assets/images/projects/vibenow.png';
-import dynamicmenu from '../../assets/images/projects/dynamicmenu.png';
+// import dynamicmenu from '../../assets/images/projects/dynamicmenu.png';
 
 
-import nationalMentoring from '../../assets/images/projects/Capture.PNG';
-import restuarant from '../../assets/images/projects/Capture1.PNG';
+import nationalMentoring from '../../assets/images/projects/_Capture.PNG';
+import restuarant from '../../assets/images/projects/_Capture1.PNG';
 
 export const web = [
 	// {
@@ -121,62 +121,62 @@ export const web = [
 ];
 
 export const design = [
-	// {
-	// 	name: 'Smart Home Dashboard UI',
-	// 	date: '2022-04-03',
-	// 	cover: smarthome,
-	// 	links: [{ icon: dribbbleLink, link: 'https://dribbble.com/shots/17912071-Smart-Home-Dashboard-UI' }],
-	// },
-	// {
-	// 	name: 'God of War Concept UI',
-	// 	date: '2022-03-13',
-	// 	cover: godofwar,
-	// 	links: [{ icon: dribbbleLink, link: 'https://dribbble.com/shots/17710279-God-of-War-Website-design' }],
-	// },
-	// {
-	// 	name: 'In time Logo',
-	// 	date: '2020-04-20',
-	// 	cover: itlogo,
-	// 	links: [{ icon: dribbbleLink, link: 'https://dribbble.com/shots/16788202-IN-TIME-Logo-design' }],
-	// },
-	// {
-	// 	name: 'NY Open Offices Logo',
-	// 	date: '2018-05-12',
-	// 	cover: nyoologo,
-	// 	links: [{ icon: dribbbleLink, link: 'https://dribbble.com/shots/18038998-New-York-Open-Offices-Logo-design' }],
-	// },
-	// {
-	// 	name: 'Midnight Pulse Logo',
-	// 	date: '2018-07-17',
-	// 	cover: mplogo,
-	// 	links: [{ icon: dribbbleLink, link: 'https://dribbble.com/shots/18302655-Midnight-Pulse-Logo-design' }],
-	// },
-	// {
-	// 	name: 'Discover Romania Concept UI',
-	// 	date: '2022-06-05',
-	// 	cover: discoverro,
-	// 	links: [
-	// 		{ icon: dribbbleLink, link: 'https://dribbble.com/shots/18413927-Discover-Romania-Landing-page' },
-	// 		{ icon: ytLink, link: 'https://www.youtube.com/watch?v=vzyxaw_ryyQ' },
-	// 	],
-	// },
-	// {
-	// 	name: 'Vice City Conceput UI',
-	// 	date: '2022-08-14',
-	// 	cover: vclp,
-	// 	links: [
-	// 		{ icon: dribbbleLink, link: 'https://dribbble.com/shots/19100552-Welcome-back-to-Vice-City-Landing-page' },
-	// 		{ icon: ytLink, link: 'https://www.youtube.com/watch?v=ysBkWUQMLHQ' },
-	// 	],
-	// },
-	// {
-	// 	name: 'Vibe Now UI',
-	// 	date: '2022-10-28',
-	// 	cover: vibenow,
-	// 	links: [
-	// 		{ icon: dribbbleLink, link: 'https://dribbble.com/shots/20106594-Vibe-Now-Mobile-app-for-events' },
-	// 	],
-	// },
+	{
+		name: 'Smart Home Dashboard UI',
+		date: '2022-04-03',
+		cover: smarthome,
+		links: [{ icon: dribbbleLink, link: 'https://dribbble.com/shots/17912071-Smart-Home-Dashboard-UI' }],
+	},
+	{
+		name: 'God of War Concept UI',
+		date: '2022-03-13',
+		cover: godofwar,
+		links: [{ icon: dribbbleLink, link: 'https://dribbble.com/shots/17710279-God-of-War-Website-design' }],
+	},
+	{
+		name: 'In time Logo',
+		date: '2020-04-20',
+		cover: itlogo,
+		links: [{ icon: dribbbleLink, link: 'https://dribbble.com/shots/16788202-IN-TIME-Logo-design' }],
+	},
+	{
+		name: 'NY Open Offices Logo',
+		date: '2018-05-12',
+		cover: nyoologo,
+		links: [{ icon: dribbbleLink, link: 'https://dribbble.com/shots/18038998-New-York-Open-Offices-Logo-design' }],
+	},
+	{
+		name: 'Midnight Pulse Logo',
+		date: '2018-07-17',
+		cover: mplogo,
+		links: [{ icon: dribbbleLink, link: 'https://dribbble.com/shots/18302655-Midnight-Pulse-Logo-design' }],
+	},
+	{
+		name: 'Discover Romania Concept UI',
+		date: '2022-06-05',
+		cover: discoverro,
+		links: [
+			{ icon: dribbbleLink, link: 'https://dribbble.com/shots/18413927-Discover-Romania-Landing-page' },
+			// { icon: ytLink, link: 'https://www.youtube.com/watch?v=vzyxaw_ryyQ' },
+		],
+	},
+	{
+		name: 'Vice City Conceput UI',
+		date: '2022-08-14',
+		cover: vclp,
+		links: [
+			{ icon: dribbbleLink, link: 'https://dribbble.com/shots/19100552-Welcome-back-to-Vice-City-Landing-page' },
+			// { icon: ytLink, link: 'https://www.youtube.com/watch?v=ysBkWUQMLHQ' },
+		],
+	},
+	{
+		name: 'Vibe Now UI',
+		date: '2022-10-28',
+		cover: vibenow,
+		links: [
+			{ icon: dribbbleLink, link: 'https://dribbble.com/shots/20106594-Vibe-Now-Mobile-app-for-events' },
+		],
+	},
 ];
 
 export const game = [
